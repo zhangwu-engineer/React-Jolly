57632031a5cfc8a00cb181fe70428b492e9b943a
Zhang Wu <zhangwu.engineer@gmail.com>
2018-10-27 05:12:46 +0900

    integrate social login into sign in page

--- app/pages/SignIn/index.js
+++ app/pages/SignIn/index.js
@@ -1,15 +1,52 @@
 // @flow
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import CONFIG from 'conf';
 
+import { history } from 'components/ConnectedRouter';
 import Link from 'components/Link';
 import Button from 'components/Button';
+import SocialButton from 'components/SocialButton';
+
+import { requestSocialLogin } from 'containers/App/sagas';
 
 import Logo from 'images/logo.png';
 
 import './styles.scss';
 
-class SignIn extends Component<{}> {
+type Props = {
+  user: Object,
+  requestSocialLogin: Function,
+};
+
+class SignIn extends Component<Props> {
+  componentDidUpdate(prevProps: Props) {
+    const { user } = this.props;
+    if (!prevProps.user && user) {
+      history.push('/me');
+    }
+  }
+  handleFBLogin = (user: Object) => {
+    const {
+      _token: { accessToken },
+    } = user;
+    this.props.requestSocialLogin({ access_token: accessToken }, 'facebook');
+  };
+
+  handleFBLoginFailure = (err: any) => {
+    console.log(err); // eslint-disable-line
+  };
+  handleLinkedInLogin = (user: Object) => {
+    const {
+      _token: { accessToken },
+    } = user;
+    this.props.requestSocialLogin({ access_token: accessToken }, 'linkedin');
+  };
+
+  handleLinkedInLoginFailure = (err: any) => {
+    console.log(err); // eslint-disable-line
+  };
   render() {
     return (
       <div className="signin">
@@ -19,12 +56,24 @@ class SignIn extends Component<{}> {
             <h1 className="signin__title mb-xl">
               Sign in to your Jolly account
             </h1>
-            <Button className="signin__btn purple-blue mb-md" type="button">
+            <SocialButton
+              provider="facebook"
+              appId={CONFIG.FACEBOOK.APP_ID}
+              onLoginSuccess={this.handleFBLogin}
+              onLoginFailure={this.handleFBLoginFailure}
+              className="signin__btn purple-blue mb-md"
+            >
               Sign In with facebook
-            </Button>
-            <Button className="signin__btn secondary nm" type="button">
+            </SocialButton>
+            <SocialButton
+              provider="linkedin"
+              appId={CONFIG.LINKEDIN.APP_ID}
+              onLoginSuccess={this.handleLinkedInLogin}
+              onLoginFailure={this.handleLinkedInLoginFailure}
+              className="signin__btn secondary nm"
+            >
               Sign In with linkedin
-            </Button>
+            </SocialButton>
             <h1 className="signin__divider">or</h1>
             <Button className="signin__btn" element={Link} to="/email-sign-in">
               Sign in with email
@@ -48,4 +97,18 @@ class SignIn extends Component<{}> {
   }
 }
 
-export default SignIn;
+const mapStateToProps = state => ({
+  user: state.getIn(['app', 'user']),
+  isSocialLoading: state.getIn(['app', 'isSocialLoading']),
+  socialError: state.getIn(['app', 'socialError']),
+});
+
+const mapDispatchToProps = dispatch => ({
+  requestSocialLogin: (payload, type) =>
+    dispatch(requestSocialLogin(payload, type)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignIn);
