7b6b9221b00b1343154d9fface49a2401a85d027
Zhang Wu <zhangwu.engineer@gmail.com>
2018-11-10 20:23:38 +0900

    style personal information page for small device

--- app/pages/PersonalInformation/index.js
+++ app/pages/PersonalInformation/index.js
@@ -102,6 +102,7 @@ const styles = theme => ({
     [theme.breakpoints.down('xs')]: {
       boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.08)',
       borderRadius: 3,
+      padding: '10px 15px',
     },
   },
 });
