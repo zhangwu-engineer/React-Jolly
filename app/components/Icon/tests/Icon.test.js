c27dd5efe9fd1c6e22a6e4e1726dde53da09be82
Zhang Wu <zhangwu.engineer@gmail.com>
2018-12-10 05:07:54 +0900

    clean code

--- app/components/Icon/tests/Icon.test.js
+++ app/components/Icon/tests/Icon.test.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Icon from 'components/Icon';
-import vapeIcon from 'images/sprite/vape.svg';
+import facebookIcon from 'images/sprite/facebook.svg';
 
 describe('<Icon />', () => {
   it('should render correct svg', () => {
-    const renderedComponent = shallow(<Icon glyph={vapeIcon} />);
+    const renderedComponent = shallow(<Icon glyph={facebookIcon} />);
     expect(renderedComponent.find('svg').prop('viewBox')).toEqual(
-      vapeIcon.viewBox
+      facebookIcon.viewBox
     );
     expect(renderedComponent.find('use').prop('xlinkHref')).toEqual(
-      `#${vapeIcon.id}`
+      `#${facebookIcon.id}`
     );
   });
   it('should have correct class from prop `className`', () => {
     const renderedComponent = shallow(
-      <Icon className="Hello" glyph={vapeIcon} />
+      <Icon className="Hello" glyph={facebookIcon} />
     );
     expect(renderedComponent.hasClass('Hello')).toEqual(true);
   });
   it('should have proper width and height', () => {
     const renderedComponent = shallow(
-      <Icon glyph={vapeIcon} width={20} height={25} />
+      <Icon glyph={facebookIcon} width={20} height={25} />
     );
     expect(renderedComponent.find('svg').prop('width')).toEqual(20);
     expect(renderedComponent.find('svg').prop('height')).toEqual(25);
   });
   it('should have proper size', () => {
-    const renderedComponent = shallow(<Icon glyph={vapeIcon} size={30} />);
+    const renderedComponent = shallow(<Icon glyph={facebookIcon} size={30} />);
     expect(renderedComponent.find('svg').prop('width')).toEqual(30);
     expect(renderedComponent.find('svg').prop('height')).toEqual(30);
   });
