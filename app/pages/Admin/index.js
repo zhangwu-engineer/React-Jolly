ca1a1ac2e3b7a8c2dddb86a1a9c67ac8d9dff036
Zhang Wu <zhangwu.engineer@gmail.com>
2019-04-15 08:22:07 -0700

    add left menu in admin panel

--- app/pages/Admin/index.js
+++ app/pages/Admin/index.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { withStyles } from '@material-ui/core/styles';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
+import { history } from 'components/ConnectedRouter';
 import Header from 'components/Header/Admin';
 import { logoutAdmin } from 'containers/App/sagas';
+import Logo from 'images/logo.png';
 import Routes from './routes';
 
 const styles = theme => ({
@@ -17,6 +22,29 @@ const styles = theme => ({
     [theme.breakpoints.down('xs')]: {
       display: 'none',
     },
+  },
+  topline: {
+    height: 70,
+    backgroundColor: theme.palette.common.white,
+    position: 'relative',
+    boxShadow: '0 5px 19px 0 rgba(0, 0, 0, 0.07)',
+    paddingLeft: 20,
+    paddingTop: 10,
+    [theme.breakpoints.down('xs')]: {
+      height: 48,
+      padding: 0,
+    },
+  },
+  menuList: {
+    padding: 0,
+  },
+  menuItem: {
+    paddingLeft: 30,
+  },
+  menuItemText: {
+    fontSize: 20,
+    fontWeight: 600,
+    color: '#343434',
   },
   content: {
     flex: 1,
@@ -39,7 +67,24 @@ class Admin extends Component<Props> {
     } = this.props;
     return (
       <div className={classes.root}>
-        <div className={classes.sidebar} />
+        <div className={classes.sidebar}>
+          <div className={classes.topline}>
+            <img src={Logo} alt="logo" />
+          </div>
+          <MenuList className={classes.menuList}>
+            <MenuItem
+              className={classes.menuItem}
+              onClick={() => {
+                history.push('/admin');
+              }}
+            >
+              <ListItemText
+                classes={{ primary: classes.menuItemText }}
+                primary="Users"
+              />
+            </MenuItem>
+          </MenuList>
+        </div>
         <div className={classes.content}>
           <Header
             user={user}
