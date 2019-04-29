14746e76457456a537b4b827de918ab6d17aeb5d
Zhang Wu <zhangwu.engineer@gmail.com>
2019-04-29 20:49:55 +0900

    update notification component

--- app/components/Notification/index.js
+++ app/components/Notification/index.js
@@ -3,15 +3,22 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
+import Icon from 'components/Icon';
+import CheckIcon from 'images/sprite/white_checkmark.svg';
+
 const styles = () => ({
   root: {
     backgroundColor: '#14a384',
     padding: '15px 25px',
   },
   content: {
-    maxWidth: 830,
+    maxWidth: 1064,
     margin: '0 auto',
     display: 'flex',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: 10,
   },
   text: {
     fontSize: 14,
@@ -38,6 +45,7 @@ class Notification extends Component<Props> {
     return (
       <div className={classes.root}>
         <div className={classes.content}>
+          <Icon glyph={CheckIcon} className={classes.icon} />
           <Typography className={classes.text}>{msg}</Typography>
         </div>
       </div>
