dba46ab1b2d6f34e5424ae93d66935f27ae3e44f
Zhang Wu <zhangwu.engineer@gmail.com>
2019-05-05 05:38:23 +0900

    create badge component

---
+++ app/components/Badge/index.js
@@ -0,0 +1,80 @@
+// @flow
+
+import React, { Component } from 'react';
+import cx from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Icon from 'components/Icon';
+import CityFreelancerIcon from 'images/sprite/city_freelancer.svg';
+import ActiveFreelancerIcon from 'images/sprite/active_freelancer.svg';
+import ReadyAndWillingIcon from 'images/sprite/ready_and_willing.svg';
+import ConnectedIcon from 'images/sprite/connected.svg';
+
+const styles = theme => ({
+  root: {
+    marginBottom: 15,
+    cursor: 'pointer',
+  },
+  label: {
+    fontWeight: 500,
+    color: theme.palette.primary.main,
+    paddingLeft: 10,
+  },
+  earnedLabel: {
+    color: '#363636',
+  },
+});
+
+type Props = {
+  badge: Object,
+  classes: Object,
+};
+
+class Badge extends Component<Props> {
+  badgeIcon = () => {
+    const { badge } = this.props;
+    const badgeName = badge.get('name');
+    if (badgeName === 'city_freelancer') {
+      return CityFreelancerIcon;
+    } else if (badgeName === 'active_freelancer') {
+      return ActiveFreelancerIcon;
+    } else if (badgeName === 'ready_and_willing') {
+      return ReadyAndWillingIcon;
+    }
+    return ConnectedIcon;
+  };
+  badgeLabel = () => {
+    const { badge } = this.props;
+    const badgeName = badge.get('name');
+    if (badgeName === 'city_freelancer') {
+      return 'Freelancer';
+    } else if (badgeName === 'active_freelancer') {
+      return 'Active Freelancer';
+    } else if (badgeName === 'ready_and_willing') {
+      return 'Ready & Willing';
+    }
+    return 'Connected';
+  };
+  render() {
+    const { badge, classes } = this.props;
+    return (
+      <Grid container className={classes.root} alignItems="center">
+        <Grid item>
+          <Icon glyph={this.badgeIcon()} size={40} />
+        </Grid>
+        <Grid item>
+          <Typography
+            className={cx(classes.label, {
+              [classes.earnedLabel]: badge.get('earned'),
+            })}
+          >
+            {this.badgeLabel()}
+          </Typography>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default withStyles(styles)(Badge);
