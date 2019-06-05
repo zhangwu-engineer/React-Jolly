0f93fb936bce7be83ac3ffda01fdc43d8656b93c
lalit-acuments <lalit@acuments.com>
2019-06-05 18:26:36 +0530

    Initial commit public profile

--- app/pages/Member/index.js
+++ app/pages/Member/index.js
@@ -640,6 +640,8 @@ class Member extends Component<Props, State> {
                 user={member}
                 endorsements={endorsements}
                 publicMode={!isPrivate}
+                currentUser={currentUser}
+                coworkers={coworkers}
               />
               <UserWorkList
                 works={works}
