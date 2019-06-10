d625a853e2307ac1013b03d6736a06a386c2fbf7
Ronak Gothi <ronakjain90@gmail.com>
2019-06-10 17:52:01 +0530

    Handling BusinessView of Network page

--- app/pages/Network/index.js
+++ app/pages/Network/index.js
@@ -249,6 +249,7 @@ type Props = {
   requestRemoveConnection: Function,
   requestAcceptConnection: Function,
   requestConnections: Function,
+  currentUser: Object,
 };
 
 type State = {
@@ -457,7 +458,14 @@ class NetworkPage extends Component<Props, State> {
     );
   };
   render() {
-    const { coworkers, connections, cityUsers, classes, total } = this.props;
+    const {
+      coworkers,
+      connections,
+      cityUsers,
+      classes,
+      total,
+      currentUser,
+    } = this.props;
     const {
       isFormOpen,
       selectedUser,
@@ -644,6 +652,7 @@ class NetworkPage extends Component<Props, State> {
         <VouchInviteFormModal
           isOpen={isFormOpen}
           user={selectedUser}
+          currentUser={currentUser}
           onCloseModal={this.closeFormModal}
           onInvite={this.handleConnectionInvite}
         />
@@ -653,6 +662,7 @@ class NetworkPage extends Component<Props, State> {
 }
 
 const mapStateToProps = state => ({
+  currentUser: state.getIn(['app', 'user']),
   user: state.getIn(['app', 'user']),
   cityUsers: state.getIn(['app', 'cityUsers', 'users']),
   total: state.getIn(['app', 'cityUsers', 'total']),
