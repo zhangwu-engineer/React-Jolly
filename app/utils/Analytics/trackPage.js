cefde7e48dec4fa997a35972b3a9e9f1f3228af0
Ronak Gothi <ronakjain90@gmail.com>
2019-07-05 10:53:36 +0530

    Segment clubbing Dynamic Pages to make single unique event.

---
+++ app/utils/Analytics/trackPage.js
@@ -0,0 +1,28 @@
+class TrackPage {
+  static send(location, user) {
+    if (
+      location.pathname.split('/').length === 3 &&
+      location.pathname.startsWith('/f/')
+    ) {
+      analytics.page('User Profile', {
+        viewer:
+          user && user.get('slug') === location.pathname.split('/').slice(-1)[0]
+            ? 'this-user'
+            : 'other-user',
+      });
+    } else if (
+      location.pathname.split('/').length === 3 &&
+      location.pathname.startsWith('/b/')
+    ) {
+      analytics.page('Business Profile Page Viewed');
+    } else if (location.pathname.startsWith('/reset-password/')) {
+      analytics.page('Reset Password Page Viewed');
+    } else if (location.pathname.startsWith('/email-verification/')) {
+      analytics.page('Email Verification Page Viewed');
+    } else {
+      analytics.page(location.pathname);
+    }
+  }
+}
+
+export default TrackPage;
