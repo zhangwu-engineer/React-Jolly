3110c2b446891f03e93d6a5ab1160eee82ac1745
tal <>
2019-07-23 07:51:03 -0400

    init businee profile form

--- app/components/BusinessProfileForm/index.js
+++ app/components/BusinessProfileForm/index.js
@@ -7,7 +7,8 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
-import EditableInput from 'components/EditableInput';
+import EditableFormInput from 'components/EditableFormInput';
+import CustomSelect from 'components/CustomSelect';
 
 const styles = () => ({
   saveButton: {
@@ -18,7 +19,17 @@ const styles = () => ({
     borderRadius: 0,
     marginTop: 5,
   },
+  searchInputWrapper: {
+    position: 'relative',
+    marginBottom: 15,
+  },
 });
+
+const placeSuggestions = [
+  { label: 'Austin, Tx', value: 'Austin, Tx' },
+  { label: 'Toledo, Spain', value: 'Toledo, Spain' },
+  { label: 'Tokyo, Japan', value: 'Tokyo, Japan' },
+];
 
 type Props = {
   business: Object,
@@ -27,19 +38,26 @@ type Props = {
 };
 
 class BusinessProfileForm extends Component<Props> {
+  state = {
+    selectedCity: null,
+  };
   handleSave = (values, { resetForm }) => {
-    const { name } = values;
     const { business } = this.props;
     this.props.updateBusiness({
       id: business && business.id,
-      name,
+      ...values,
     });
     resetForm(values);
   };
+  handleRoleChange = city => {
+    this.setState({ selectedCity: city });
+  };
   render() {
     const { business, classes } = this.props;
+    const { selectedCity } = this.state;
     const businessData = {
       name: business.name,
+      location: business.location,
     };
     return (
       <Formik initialValues={businessData} onSubmit={this.handleSave}>
@@ -55,10 +73,54 @@ class BusinessProfileForm extends Component<Props> {
           /* and other goodies */
         }) => (
           <React.Fragment>
-            <EditableInput
+            <EditableFormInput
               label="Name"
               id="name"
               value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <EditableFormInput
+              label="Headquaters City"
+              id="location"
+              value={values.location}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <Grid item xs={12} lg={12} className={classes.searchInputWrapper}>
+              <CustomSelect
+                placeholder="Other Cities Active"
+                options={placeSuggestions}
+                value={selectedCity}
+                onChange={value => this.handleRoleChange(value)}
+                isMulti
+                isClearable={false}
+                stylesOverride={{
+                  container: () => ({
+                    backgroundColor: '#f1f1f1',
+                    width: '100%',
+                  }),
+                }}
+              />
+            </Grid>
+            <EditableFormInput
+              label="About Us"
+              id="aboutUs"
+              value={values.aboutUs}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <EditableFormInput
+              label="Website"
+              id="website"
+              value={values.website}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <EditableFormInput
+              label="Freelancer Payment Terms"
+              id="freelancerPaymentTerms"
+              value={values.freelancerPaymentTerms}
               onChange={handleChange}
               onBlur={handleBlur}
             />
